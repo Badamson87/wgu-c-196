e4af554742a96ff666be44e43b481af233dad3f9
brian <brianadamson87@gmail.com>
2021-01-06 08:37:08 -0700

    1. Term Model 2. Course model 3. Term DAO

---
+++ app/src/main/java/Daos/TermDao.java
@@ -0,0 +1,36 @@
+package Daos;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import Models.Term;
+
+@Dao
+public interface TermDao {
+    @Query("SELECT * FROM term_table ORDER BY term_id")
+    List<Term> getTermList();
+
+    @Query("SELECT * FROM term_table WHERE term_id = :termId ORDER BY term_id")
+    Term getTermById(int termId);
+
+    @Query("SELECT * FROM term_table")
+    List<Term> getAllTerms();
+
+    @Insert
+    void insertTerm(Term term);
+
+    @Update
+    void updateTerm(Term term);
+
+    @Delete
+    void deleteTerm(Term term);
+
+    @Query("DELETE FROM term_table")
+    public void nukeTermTable();
+
+}
