d48f4c279a46967101b30204bde6b5ca3929f336
brian <brianadamson87@gmail.com>
2021-01-05 21:18:35 -0700

    Create MyTermActivity and AllTermsActivity 2. create my term button and nav to my term activity with identifying text

---
+++ app/src/main/java/wgu/c_196/AllTermsActivity.java
@@ -0,0 +1,14 @@
+package wgu.c_196;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllTermsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_terms);
+    }
+}
