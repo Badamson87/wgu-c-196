3a64c4de35bbff82cf18105d951252709f11d964
brian <brianadamson87@gmail.com>
2021-01-09 21:34:20 -0700

    create a term and persist in DB. 2. display a list of term names in the all terms view.

--- app/src/main/java/wgu/c_196/AddTermActivity.java
+++ app/src/main/java/wgu/c_196/AddTermActivity.java
@@ -3,12 +3,56 @@ package wgu.c_196;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import Helpers.Database;
+import Models.Term;
 
 public class AddTermActivity extends AppCompatActivity {
+    EditText termNameInput;
+    EditText termStartInput;
+    EditText termEndInput;
+    Button  saveTermButton;
+    Database db;
+    SimpleDateFormat formatter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_term);
+        db = Database.getInstance(getApplicationContext());
+        saveTermButton = findViewById(R.id.saveTermButton);
+        termNameInput = findViewById(R.id.termNameInput);
+        termStartInput = findViewById(R.id.termStartInput);
+        termEndInput = findViewById(R.id.termendInput);
+        formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        saveTermButton.setOnClickListener((e) -> {saveTerm();});
+    }
+
+    private void saveTerm(){
+        System.out.println("save term clicked");
+        System.out.println(termNameInput.getText().toString());
+        System.out.println(termStartInput.getText().toString());
+        System.out.println(termEndInput.getText().toString());
+     if (termNameInput != null && termStartInput != null && termEndInput !=null)
+     {
+         System.out.println("got past save term null checks");
+         Date start = new Date(termStartInput.getText().toString());
+         Date end = new Date(termEndInput.getText().toString());
+         Term term = new Term();
+         term.setTerm_name(termNameInput.getText().toString());
+         term.setTerm_start(start);
+         term.setTerm_end(end);
+         db.termDao().insertTerm(term);
+     }
+        System.out.println("starting term print from add term");
+        db.termDao().getAllTerms().forEach((term -> {
+            System.out.println(term.getTerm_name());
+        }));
     }
 }
--- app/src/main/java/wgu/c_196/AllTermsActivity.java
+++ app/src/main/java/wgu/c_196/AllTermsActivity.java
@@ -4,15 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import Helpers.Database;
+import Models.Term;
 
 public class AllTermsActivity extends AppCompatActivity {
-
+    ListView allTermsList;
+    Database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_terms);
+        db = Database.getInstance(getApplicationContext());
+        allTermsList = findViewById(R.id.allTermsList);
+        this.getAllTerms();
+        System.out.println("starting term print from all terms");
+        db.termDao().getAllTerms().forEach((term -> {
+          System.out.println(term.getTerm_name());
+      }));
+    }
+
+    private void getAllTerms(){
+        List<Term> terms = db.termDao().getAllTerms();
+        String[] items = new String[terms.size()];
+        for (int i = 0; i < terms.size(); i++){
+            items[i] = terms.get(i).getTerm_name();
+        }
+//        terms.forEach(size(term -> {
+//            termStringArray.push()
+//        }));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        allTermsList.setAdapter(adapter);
     }
 
     public void addTermScreen(View view) {
