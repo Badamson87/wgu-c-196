fd5eda235e723e2384159c1bbc6967a42d04932d
brian <brianadamson87@gmail.com>
2021-01-12 08:46:41 -0700

    Term creation with datepicker 2. routing to all terms on create

--- app/src/main/java/wgu/c_196/AddTermActivity.java
+++ app/src/main/java/wgu/c_196/AddTermActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,14 +23,12 @@ import Models.Term;
 
 public class AddTermActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     EditText termNameInput;
-    EditText termStartInput;
-    EditText termEndInput;
+    TextView termStartInput;
+    TextView termEndInput;
     Button  saveTermButton;
     Button  cancelSaveTerm;
-    Button pickerButton;
     Database db;
-    SimpleDateFormat formatter;
-
+    Boolean updateStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,22 +39,33 @@ public class AddTermActivity extends AppCompatActivity implements DatePickerDial
         cancelSaveTerm = findViewById(R.id.cancelSaveTerm);
         termNameInput = findViewById(R.id.termNameInput);
         termStartInput = findViewById(R.id.termStartInput);
-        termEndInput = findViewById(R.id.termendInput);
-        pickerButton = findViewById(R.id.pickerButton);
-        formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        termEndInput = findViewById(R.id.termEndInput);
+        termStartInput.setOnClickListener((e) -> updateStart());
+        termEndInput.setOnClickListener((e) -> updateEnd());
         saveTermButton.setOnClickListener((e) -> {saveTerm();});
-        cancelSaveTerm.setOnClickListener((e) -> cancelSaveTerm());
-        pickerButton.setOnClickListener((e) -> displayPicker());
+        cancelSaveTerm.setOnClickListener((e) -> sendToAllTerms());
+    }
+
+    private void updateStart(){
+        this.updateStart = true;
+        displayPicker();
+    }
+
+    private void updateEnd(){
+        this.updateStart = false;
+        displayPicker();
     }
 
     private void displayPicker() {
         DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "startPicker");
+        newFragment.show(getSupportFragmentManager(), "termPicker");
     }
 
 
     private void saveTerm() {
-        if (termNameInput.getText().toString().equals("") || termStartInput.getText().toString().equals("") || termEndInput.getText().toString().equals("")){
+        if (termNameInput.getText().toString().equals("") || termStartInput.getText().toString().contains("Click") || termEndInput.getText().toString().contains("C")){
+            Toast toast = Toast.makeText(getApplicationContext(), "Please complete form.", Toast.LENGTH_SHORT);
+            toast.show();
             return;
         }
         Date start = new Date(termStartInput.getText().toString());
@@ -62,9 +75,10 @@ public class AddTermActivity extends AppCompatActivity implements DatePickerDial
         term.setTerm_start(start);
         term.setTerm_end(end);
         db.termDao().insertTerm(term);
+        sendToAllTerms();
     }
 
-    public void cancelSaveTerm(){
+    public void sendToAllTerms(){
         Intent intent = new Intent(AddTermActivity.this, AllTermsActivity.class);
         startActivity(intent);
     }
@@ -72,6 +86,11 @@ public class AddTermActivity extends AppCompatActivity implements DatePickerDial
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        termStartInput.setText((month + 1) + "/" + dayOfMonth + "/" + year);
+        if (this.updateStart){
+            termStartInput.setText((month + 1) + "/" + dayOfMonth + "/" + year);
+        }
+        else {
+            termEndInput.setText((month + 1) + "/" + dayOfMonth + "/" + year);
+        }
     }
 }
